1f70be1ed61dcadc4ee90320f478ad1598d4ecb0
zalish-shawon <zalish.shawon@gmail.com>
2024-01-02 00:37:27 +0600

    sidebae added

---
+++ src/components/Home/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div className='mr-[80px] mt-10 bg-white h-[320px] w-[259px] fixed right-1 z-5'>
+            <h1>dasdasdsad</h1>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/no-unknown-property */
-import React from 'react';
+import Cart from './Cart';
 import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 
 const Home = () => {
     return (
         <div>
              <Navbar></Navbar>
+             <div className='flex justify-between'>
+                <Sidebar></Sidebar>
+                <Cart></Cart>
+             </div>
         </div>
     );
 };
